22e61c0d31196d741b459e2918425a1949ea7844
Dell <7383284@qq.com>
2022-12-14 01:39:00 +0800

    v1.0

---
+++ src/main/java/com/tools/controller/UsingStaticController.java
@@ -0,0 +1,10 @@
+//package com.tools.controller;
+//
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+//public class UsingStaticController extends WebMvcConfigurationSupport {
+//    public void addResurceHandlers(ResourceHandlerRegistry registry){
+////        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//    }
+//}
